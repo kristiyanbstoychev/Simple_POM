5092d34a765420bd7cbfe70f3c2a70c97d74c973
Kristiyan Stoychev <77746043+kristiyanbstoychev@users.noreply.github.com>
2021-04-08 21:27:39 +0300

    Update Tests.cs

--- Tests.cs
+++ Tests.cs
@@ -1,33 +1,29 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Threading;
-//Use the pageObjects class!
 using AutomationPractice.Objects;
 
 namespace AutomationPractice
 {
-        public class SendMessageTest
+        public class textBoxTests
     {
-        IWebDriver driver;
-        
+        IWebDriver driver;   
+
         [OneTimeSetUp]
         public void Setup()
         {
             driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://demoqa.com/text-box");  
+            driver.Navigate().GoToUrl("https://demoqa.com/text-box");
         }
 
         [Test]
         public void SendMessage()
-        {
-            //call the pretty pageObjects
-            driver.FindElement(pageObjects.inputFullName).SendKeys("test user");
-            driver.FindElement(pageObjects.inputEmail).SendKeys("test@test.com");
-            driver.FindElement(pageObjects.inputCurrentAddress).SendKeys("this is a test address!");
-            driver.FindElement(pageObjects.inputPermanentAddress).SendKeys("this is also a test address!");
+        {   //setup driver for the test
+            var textBoxPageDriver = new textBoxPageObjects(driver);
+            textBoxPageDriver.inputFullName.SendKeys("dasdsa");
+
         }
 
         [OneTimeTearDown]
